ba297f1f85a1f00329a2e6d47c61b357fb46cfcf
himanshu12yadav <himanshuyadav.12jan@gmail.com>
2025-08-08 17:53:05 +0530

    update:schema code

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -17,7 +17,7 @@ export default function App() {
   const { apiKey } = useLoaderData();
 
   return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
+    <AppProvider isEmbeddedApp apiKey={apiKey} hello={"Himanshu"}>
       <NavMenu>
         <Link to="/app" rel="home">
           Home
